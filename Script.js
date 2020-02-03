0beb90fd1d3e4fdc753cacf624bd6e975cfee296
CODE.BIER <59924431+a-biernacki@users.noreply.github.com>
2020-02-03 11:48:32 -0500

    Update Script.js
    2nd commit

--- Script.js
+++ Script.js
@@ -11,38 +11,49 @@ var displayResult = document.getElementById("results");
 
 //functions
 tempCToF.onclick = function(){
-    var input = document.getElementById("userInput").innerHTML;
-    var fahrResult = (input * 9/8) + 32;
-    displayResult.innerHTML=fahrResult;
-    console.log(input);
+    var input = document.getElementById("userInput").value;
+    var fahrResult = (input * 9/5) + 32;
+    displayResult.innerHTML = input + " &#8451; = " + fahrResult + " &#8457;.";
 };
 
 tempFToC.onclick = function(){
-    var input = document.getElementById("userInput").innerHTML;
+    var input = document.getElementById("userInput").value;
+    var celsResult = (input - 32) * 5/9;
+    displayResult.innerHTML = input + " &#8457; = " + celsResult + " &#8451;.";
 };
 
 lengthFToM.onclick = function(){
-    var input = document.getElementById("userInput").innerHTML;
+    var input = document.getElementById("userInput").value;
+    var meterResult = input / 3.281;
+    displayResult.innerHTML = input + "ft = " + meterResult + "m.";
 };
 
 lengthMToF.onclick = function(){
-    var input = document.getElementById("userInput").innerHTML;
+    var input = document.getElementById("userInput").value;
+    var footResult = input * 3.281;
+    displayResult.innerHTML = input + "m = " + footResult + "ft.";
 };
 
 lengthInToCm.onclick = function(){
-    var input = document.getElementById("userInput").innerHTML;
+    var input = document.getElementById("userInput").value;
+    var cmResult = input * 2.54;
+    displayResult.innerHTML = input + "in = " + cmResult + "cm."
 };
 
 lengthCmToIn.onclick = function(){
-    var input = document.getElementById("userInput").innerHTML;
+    var input = document.getElementById("userInput").value;
+    var inResult = input / 2.54;
+    displayResult.innerHTML = input + "cm = " + inResult + "in."
 };
 
 weightLbToKg.onclick = function(){
-    var input = document.getElementById("userInput").innerHTML;
+    var input = document.getElementById("userInput").value;
+    var kgResult = input / 2.205;
+    displayResult.innerHTML = input + "lbs = " + kgResult + "kgs."
 };
 
 weightKgToLb.onclick = function(){
-    var input = document.getElementById("userInput").innerHTML;
+    var input = document.getElementById("userInput").value;
+    var lbResult = input * 2.205;
+    displayResult.innerHTML = input + "kgs = " + lbResult + "lbs."
 };
-
-
